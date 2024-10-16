a4daab05d26ecaf942ac0d52b9c766a29b4c1970
Adam Spice <adam@spicey.dev>
2024-10-17 11:17:36 +1300

    feat: changed component styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/button";
-
+import { Input } from "@/components/ui/input";
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button>Click me</Button>
+    <div className="flex gap-4">
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="tertiary">Tertiary</Button>
+      <Input />
     </div>
   );
 }
